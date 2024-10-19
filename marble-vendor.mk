9eb3def6ed225d0f33c012db346ffb54585745b8
chaitanyakm <chaitanyaarora3@gmail.com>
2024-10-20 01:11:13 +0530

    marble: update blobs from 1.0.9.0.UMRMIXM

--- marble-vendor.mk
+++ marble-vendor.mk
@@ -1086,7 +1086,6 @@ PRODUCT_PACKAGES += \
     misound_res_headphone \
     misound_res_spk \
     misound_res_spk4ch \
-    vendor.xiaomi.hardware.fx.tunnel@1.0 \
     vendor.dolby.hardware.dms \
     vendor.xiaomi.hardware.quickcamera@1.0-service \
     batterysecret \
